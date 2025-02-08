35c93c2c6c679de3453192c6a603dc708dd926c9
Souravkr7909 <sauravkr5963@gmail.com>
2025-02-08 12:43:49 +0530

    First commit

---
+++ Array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+        int mark[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+        mark[0] = sc.nextInt();
+        mark[1] = sc.nextInt();
+        mark[2] = sc.nextInt();
+
+        System.out.println("phy :" + mark[0]);
+        System.out.println("Chem :" + mark[1]);
+        System.out.println("math :" + mark[2]);
+    }
+
+
+}
